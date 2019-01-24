d347c9ddca20558f9a44bb7aed49ae2b3f4230b2
Luiz Alberto <gomes.luiz@gmail.com>
2019-01-24 18:11:00 -0200

    #1: chapter 11 code updated.

--- src/main/java/capitulo11/com/refatoracao/FicharioDeEmprestimos.java
+++ src/main/java/capitulo11/com/refatoracao/FicharioDeEmprestimos.java
@@ -7,57 +7,59 @@ import java.util.List;
  * classe <code>Emprestimo</code>.
  * 
  * @author Luiz Alberto
+ * 
  * @see {@link Emprestimo}
  */
 public class FicharioDeEmprestimos {
 
-	private List<Emprestimo> emprestimos;
+  private List<Emprestimo> emprestimos;
 
-	/**
-	 * Obtem o nome do usuário que realizou um determinado empréstimo.
-	 * 
-	 * @param codigoDoEmprestimo O código do empréstimo.
-	 *
-	 * @return O nome do usuário.
-	 *
-	 * @throws EmprestimoNaoEncontradoException se o emprestimo não for encontrado.
-	 *
-	 */
-	public String getNomeDoUsuario(String codigoDoEmprestimo) throws EmprestimoNaoEncontradoException {
-		Emprestimo emprestimo = getEmprestimoPeloCodigo(codigoDoEmprestimo);
-		return (emprestimo.getNomeDoUsuario());
-	}
+  /**
+   * Obtem o nome do usuário que realizou um determinado empréstimo.
+   * 
+   * @param codigoDoEmprestimo O código do empréstimo.
+   *
+   * @return O nome do usuário.
+   *
+   * @throws EmprestimoNaoEncontradoException se o emprestimo não for encontrado.
+   *
+   */
+  public String getNomeDoUsuario(String codigoDoEmprestimo) throws EmprestimoNaoEncontradoException {
+    Emprestimo emprestimo = getEmprestimoPeloCodigo(codigoDoEmprestimo);
+    return (emprestimo.getNomeDoUsuario());
+  }
 
-	/**
-	 * Remove um objeto <code>Emprestimo</code> pelo código do empréstimo.
-	 * 
-	 * @param codigoDoEmprestimo O código do empréstimo.
-	 *
-	 * @return nenhum.
-	 *
-	 * @throws EmprestimoNaoEncontradoException se o emprestimo não for encontrado.
-	 *
-	 */
-	public void removeUmEmprestimo(String codigoDoEmprestimo) throws EmprestimoNaoEncontradoException {
-		emprestimos.remove(getEmprestimoPeloCodigo(codigoDoEmprestimo));
-	}
+  /**
+   * Remove um objeto <code>Emprestimo</code> pelo código do empréstimo.
+   * 
+   * @param codigoDoEmprestimo O código do empréstimo.
+   *
+   * @return nenhum.
+   *
+   * @throws EmprestimoNaoEncontradoException se o emprestimo não for encontrado.
+   *
+   */
+  public void removeUmEmprestimo(String codigoDoEmprestimo) throws EmprestimoNaoEncontradoException {
+    emprestimos.remove(getEmprestimoPeloCodigo(codigoDoEmprestimo));
+  }
 
-	/**
-	 * Encontra um objeto da classes <code>Emprestimo</code> pelo seu código.
-	 * 
-	 * @param codigo o código do emprestimo.
-	 *
-	 * @return um objeto da classe <code>Emprestimo</code>.
-	 * 
-	 * @throws EmprestimoNaoEncontradoException se o empréstimo não for encontrado.
-	 *
-	 */
-	private Emprestimo getEmprestimoPeloCodigo(String codigo) throws EmprestimoNaoEncontradoException {
-		for (Emprestimo emprestimo : emprestimos) {
-			if (emprestimo.getCodigo().equals(codigo)) {
-				return emprestimo;
-			}
-		}
-		throw new EmprestimoNaoEncontradoException();
-	}
+  /**
+   * Encontra um objeto da classes <code>Emprestimo</code> pelo seu código.
+   * 
+   * @param codigo o código do emprestimo.
+   *
+   * @return um objeto da classe <code>Emprestimo</code>.
+   * 
+   * @throws EmprestimoNaoEncontradoException se o empréstimo não for encontrado.
+   *
+   */
+  private Emprestimo getEmprestimoPeloCodigo(String codigo) throws EmprestimoNaoEncontradoException {
+    for (Emprestimo emprestimo : emprestimos) {
+      if (emprestimo.getCodigo().equals(codigo)) {
+        return emprestimo;
+      }
+    }
+    throw new EmprestimoNaoEncontradoException();
+
+  }
 }
